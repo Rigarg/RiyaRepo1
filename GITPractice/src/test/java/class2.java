cdd6c7fb77a6ef5a7cdb0e983536931c8a798e1a
riyag <riya.garg0607@gmail.com>
2022-07-17 21:19:38 +0530

    This is commit

--- GITPractice/src/test/java/class2.java
+++ GITPractice/src/test/java/class2.java
@@ -1,12 +1,9 @@
 
-public class class1 {
+public class class2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is class1");
-		System.out.println("This is class1");
-		System.out.println("This is class1");
-
 	}
 
 }
