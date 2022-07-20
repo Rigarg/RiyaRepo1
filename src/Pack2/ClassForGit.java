1a12e002fd363eb86d620b549b9beae099092392
Rigarg <riya.garg0607@gmail.com>
2022-07-20 12:29:34 +0530

    This is Commit  for Git merge conflict

---
+++ src/Pack2/ClassForGit.java
@@ -0,0 +1,12 @@
+package Pack2;
+
+import org.testng.annotations.Test;
+
+public class ClassForGit {
+	
+	@Test
+	public void method() {
+		System.out.println("This is method 1 for GIT push");
+	}
+
+}
